93147030d58f1efc77e00e42a5780093d8fdd65b
matronae <mykaomas@gmail.com>
2024-01-08 19:14:39 -0600

    removed some code from handlebars

--- server.js
+++ server.js
@@ -6,20 +6,20 @@ const sequelize = require('./config/connection');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Configure Handlebars as the view engine
+
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Define a default layout file for your views (optional)
+
 app.set('views', path.join(__dirname, 'views'));
 
-// Set up your routes
+
 app.use(routes);
 
-// turn on connection to db and server
+
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log(`Now listening on http://localhost:${PORT}`));
 });
